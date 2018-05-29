70ec964c0ac105b5e30bb9ab1ea395846f2ef7ee
Elana Ryznar <eryzna@gmail.com>
2018-05-29 00:31:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
 
   def self.find_or_create_by_name (song_name)
     if self.find_by_name(song_name)
-      puts song_name
+     song_name
     else
       self.create_by_name(song_name)
     end
