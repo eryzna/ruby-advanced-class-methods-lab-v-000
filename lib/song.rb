6b098448486614d44d1defe4cd1a66126ecc678c
Elana Ryznar <eryzna@gmail.com>
2018-05-29 00:13:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,11 +30,11 @@ class Song
   end
 
   def self.find_by_name(song_name)
-    self.all#.detect{|songs| song.name==song_name}
+    self.all.detect{|songs| song.name==song_name}
   end
+    
 
 
-end
 
 
 end
